b47ad46988031dc665e157a2c9bb7f9f973b33cf
reindra-ubuntu <reindrairawan@gmail.com>
2022-09-20 14:13:51 +0700

    change the TourismEntity class to Tourism

--- app/src/main/java/com/reindrairawan/tourismappdicoding/core/ui/TourismAdapter.kt
+++ app/src/main/java/com/reindrairawan/tourismappdicoding/core/ui/TourismAdapter.kt
@@ -9,15 +9,16 @@ import com.bumptech.glide.Glide
 import com.dicoding.tourismapp.core.data.source.local.entity.TourismEntity
 
 import com.reindrairawan.tourismappdicoding.R
+import com.reindrairawan.tourismappdicoding.core.domain.model.Tourism
 import com.reindrairawan.tourismappdicoding.databinding.ItemListTourismBinding
 import java.util.ArrayList
 
 class TourismAdapter : RecyclerView.Adapter<TourismAdapter.ListViewHolder>() {
 
-    private var listData = ArrayList<TourismEntity>()
-    var onItemClick: ((TourismEntity) -> Unit)? = null
+    private var listData = ArrayList<Tourism>()
+    var onItemClick: ((Tourism) -> Unit)? = null
 
-    fun setData(newListData: List<TourismEntity>?) {
+    fun setData(newListData: List<Tourism>?) {
         if (newListData == null) return
         listData.clear()
         listData.addAll(newListData)
@@ -36,7 +37,7 @@ class TourismAdapter : RecyclerView.Adapter<TourismAdapter.ListViewHolder>() {
 
     inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val binding = ItemListTourismBinding.bind(itemView)
-        fun bind(data: TourismEntity) {
+        fun bind(data: Tourism) {
             with(binding) {
                 Glide.with(itemView.context)
                     .load(data.image)
