4abac1700fc70c732b4298e9b8f82c47e1299da0
reindra-ubuntu <reindrairawan@gmail.com>
2022-09-21 10:34:17 +0700

    create interfaces for use cases

---
+++ app/src/main/java/com/reindrairawan/tourismappdicoding/core/domain/usecase/TourismUseCase.kt
@@ -0,0 +1,13 @@
+package com.reindrairawan.tourismappdicoding.core.domain.usecase
+
+import androidx.lifecycle.LiveData
+import com.dicoding.tourismapp.core.data.Resource
+import com.reindrairawan.tourismappdicoding.core.domain.model.Tourism
+
+interface TourismUseCase {
+
+    fun getAllTourism(): LiveData<Resource<List<Tourism>>>
+    fun getFavoriteTourism(): LiveData<List<Tourism>>
+    fun setFavoriteTourism(tourism: Tourism, state: Boolean)
+    
+}
