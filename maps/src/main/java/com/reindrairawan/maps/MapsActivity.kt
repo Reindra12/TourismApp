9362aeadb1828d756132dd3c96aa51510b0d4498
reindra-ubuntu <reindrairawan@gmail.com>
2022-09-27 09:24:59 +0700

    display data from Repository

--- maps/src/main/java/com/reindrairawan/maps/MapsActivity.kt
+++ maps/src/main/java/com/reindrairawan/maps/MapsActivity.kt
@@ -7,5 +7,23 @@ class MapsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+    private fun getTourismData() {
+        mapsViewModel.tourism.observe(this, { tourism ->
+            if (tourism != null) {
+                when (tourism) {
+                    is Resource.Loading -> binding.progressBar.visibility = View.VISIBLE
+                    is Resource.Success -> {
+                        binding.progressBar.visibility = View.GONE
+                        binding.tvMaps.text  = "This is map of ${tourism.data?.get(0)?.name}"
+                    }
+                    is Resource.Error -> {
+                        binding.progressBar.visibility = View.GONE
+                        binding.tvError.visibility = View.VISIBLE
+                        binding.tvError.text = tourism.message
+                    }
+                    else -> {}
+                }
+            }
+        })
     }
 }
